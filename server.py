1888019b5b1f9fba6146718b6801b25c796b3f0e
Marius Popovici <marius@popovici.net>
2017-01-01 19:57:33 -0500

    reduced description length

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
+from __future__ import division
+from flask import Flask, json, request, jsonify
 
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, json, request, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -13,15 +12,69 @@ def api_hello():
 def api_export():
     if request.headers['Content-Type'] == 'application/json':
         workout = request.get_json();
-        data = export_MRC(workout);
-        response = make_response(data);
-        response.headers["Content-Disposition"] = "attachment; filename=workout.txt"
-        return response;
+        data = export(workout);
+        if not (data is None):
+            return jsonify(status = True, content = data);
+        else:
+            return jsonify(status = False, content = None);
     else:
-        data = "{status: false, description: '415 Unsupported Media Type'}";
-        return jsonify(data);
+        return jsonify(status = False, content = "415 Unsupported Media Type");
     return;
 
+def export(workout):
+    format = workout['Format'];
+    workout = workout['Workout'];
+    if (format == "MRC"):
+        return export_MRC(workout);
+    else:
+        if (format == "ERG"):
+            return export_ERG(workout);
+        else:
+            return "";
 
 def export_MRC(workout):
-    return;
+    content = ("[COURSE HEADER]\nVERSION = 2\nUNITS = ENGLISH\n"
+        "DESCRIPTION = $DESCRIPTION$\n"
+        "FILE NAME = $FILENAME$\n"
+        "MINUTES PERCENT\n[END COURSE HEADER]\n[COURSE DATA]\n");
+
+    content = content.replace("$DESCRIPTION$", workout['name']);
+    content = content.replace("$FILENAME$", workout['name'] + '.MRC');
+
+    for group in workout['groups']:
+        for repeat in range(0, group['repeat']):
+            for step in group['steps']:
+                duration = step['duration'];
+                if (step['timeUnits'] == "sec"):
+                    duration = step['duration'] * 1/60;
+                    duration = round(duration, 2);
+                content += str(duration) + " " + str(step['intensity']) + "\n";
+
+    content += "[END COURSE DATA]";
+
+    return content;
+
+def export_ERG(workout):
+    content = ("[COURSE HEADER]\nVERSION = 2\nUNITS = ENGLISH\n"
+        "DESCRIPTION = $DESCRIPTION$\n"
+        "FILE NAME = $FILENAME$\nFTP=$FTP$\n"
+        "MINUTES WATTS\n[END COURSE HEADER]\n[COURSE DATA]\n");
+
+    content = content.replace("$DESCRIPTION$", workout['name']);
+    content = content.replace("$FILENAME$", workout['name'] + '.ERG');
+    content = content.replace("$FTP$", str(workout['FTP']));
+
+    for group in workout['groups']:
+        for repeat in range(0, group['repeat']):
+            for step in group['steps']:
+                duration = step['duration'];
+                if (step['timeUnits'] == "sec"):
+                    duration = step['duration'] * 1/60;
+                    duration = round(duration, 2);
+                wattage = int((step['intensity'] * workout['FTP']) / 100);
+                content += str(duration) + " " + str(wattage) + "\n";
+
+    content += "[END COURSE DATA]";
+
+    return content;
+
