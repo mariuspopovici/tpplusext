5e6fe732ee132ec36de68254db0a0fc93a6bb37d
Marius Popovici <marius@popovici.net>
2017-01-03 12:03:57 -0500

    add analytics tracking

--- popup.js
+++ popup.js
@@ -25,3 +25,18 @@ function go() {
 }
 
 document.getElementById('export').addEventListener('click', go);
+
+var _AnalyticsCode = 'UA-89710256-1';
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', _AnalyticsCode]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
